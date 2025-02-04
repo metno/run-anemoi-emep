add07ad39e7ea73639cc47881a037a75b9a2d652
Håvard Homleid Haugen <148321314+havardhhaugen@users.noreply.github.com>
2025-02-04 08:36:43 +0100

    Update env_setup.sh
    Dont set metno remote for ecmwf repo

--- lumi/env_setup.sh
+++ lumi/env_setup.sh
@@ -23,9 +23,6 @@ for repo in utils; do
     if [ ! -d anemoi-$repo ]; then
         echo "Cloning $repo"
         git clone https://github.com/ecmwf/anemoi-$repo.git
-        cd anemoi-$repo
-        git remote set-url origin git@github.com:metno/anemoi-$repo.git
-        cd ..
     fi
     pip install --user --no-deps -e anemoi-$repo
 done
