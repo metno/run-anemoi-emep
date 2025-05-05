0bc464e1ccdd1fbcf318082f6b0addd85d0357c9
Erik Askov Mousing <10355482+eamousing@users.noreply.github.com>
2025-05-05 13:36:27 +0300

    Remove non-existing library from run script

--- lumi/lumi_jobscript.sh
+++ lumi/lumi_jobscript.sh
@@ -3,27 +3,27 @@
 #SBATCH --error=/users/%u/%x_%j.err
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
-#SBATCH --account=project_465001383
+#SBATCH --account=project_465001905
 #SBATCH --partition=standard-g
 #SBATCH --gpus-per-node=8
 #SBATCH --time=24:00:00
-#SBATCH --job-name=aifs
+#SBATCH --job-name=kl-aq-anemoi
 #SBATCH --exclusive
 
 
 #Change this
-CONFIG_NAME=CONFIGNAME.yaml #This file should be located in run-anemoi/lumi
+CONFIG_NAME=/users/mousinge/Repos/emep-anemoi/training/config3/config.yaml #This file should be located in run-anemoi/lumi
 
 #Should not have to change these
 PROJECT_DIR=/scratch/$SLURM_JOB_ACCOUNT
 CONTAINER_SCRIPT=$(pwd -P)/run_pytorch.sh
 chmod 770 ${CONTAINER_SCRIPT}
 CONFIG_DIR=$(pwd -P)
-CONTAINER=$PROJECT_DIR/aifs/container/containers/anemoi-training-pytorch-2.2.2-rocm-5.6.1-py-3.11.5.sif
+CONTAINER=$PROJECT_DIR/container/lumi-rocm-6.2.2-python-3.12-pytorch-2.5.1-kl-aq-anemoi.sif
 VENV=$(pwd -P)/.venv
 export VIRTUAL_ENV=$VENV
 
-module load LUMI/23.09 partition/G
+module load LUMI partition/G
 export SINGULARITYENV_LD_LIBRARY_PATH=/opt/ompi/lib:${EBROOTAWSMINOFIMINRCCL}/lib:/opt/cray/xpmem/2.4.4-2.3_9.1__gff0e1d9.shasta/lib64:${SINGULARITYENV_LD_LIBRARY_PATH}
 
 # MPI + OpenMP bindings: https://docs.lumi-supercomputer.eu/runjobs/scheduled-jobs/distribution-binding
@@ -36,6 +36,5 @@ srun --cpu-bind=$CPU_BIND \
                      -B /var/spool/slurmd \
                      -B /opt/cray \
                      -B /usr/lib64 \
-                     -B /usr/lib64/libjansson.so.4 \
         $CONTAINER $CONTAINER_SCRIPT $CONFIG_DIR $CONFIG_NAME
 
